ac4317e0d30e2fc2d2aedf8c8b4d25d674267633
shahnavaz173 <shahnavz73@gmail.com>
2025-10-18 17:36:02 +0530

    Update firebase config to new project

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,20 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYmNddILN6-Zy_5uvyu_aHX0UMidWs_cA",
-  authDomain: "fin-zaar.firebaseapp.com",
-  projectId: "fin-zaar",
-  storageBucket: "fin-zaar.firebasestorage.app",
-  messagingSenderId: "295221975101",
-  appId: "1:295221975101:web:28e57beb279efa3f58f254",
-  measurementId: "G-VFQVVS4CBM"
+  apiKey: "AIzaSyDKqHLMA_VvkZf40QE3wUlWaLL15xOTfCo",
+  authDomain: "fund-flare.firebaseapp.com",
+  projectId: "fund-flare",
+  storageBucket: "fund-flare.firebasestorage.app",
+  messagingSenderId: "653988636891",
+  appId: "1:653988636891:web:2159230faeb895378604fe",
+  measurementId: "G-0N978P91SJ"
 };
 
 // Initialize Firebase
